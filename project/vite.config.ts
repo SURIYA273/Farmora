c91537a1ea479f4313cbd17351c7788e766dd457
SURIYA273 <suriyaprama1234@gamil.com>
2025-04-13 20:48:37 +0530

    Fixed merge conflict in vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // 👇 Add the "base" property here
 export default defineConfig({
-<<<<<<< HEAD
-  base: '/Farmora/',  // Important: This must match your repo name
-=======
+
   base: '/Farmora/', // 👈 Add this line
->>>>>>> ca220adc8a2e5b553cea409557932d69110b9e14
+
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
